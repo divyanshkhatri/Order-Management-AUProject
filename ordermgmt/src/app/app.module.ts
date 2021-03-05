21cc5ae81af2cdd478aaefb9d5fc1502e5ffb339
yashwanthmukka <yashwanthmukka15@gmail.com>
2021-03-05 12:03:38 +0530

    first

---
+++ ordermgmt/src/app/app.module.ts
@@ -0,0 +1,61 @@
+import { RouterModule } from '@angular/router';
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { MatSliderModule } from '@angular/material/slider';
+import { MatTabsModule } from '@angular/material/tabs';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavbarComponent } from './navbar/navbar.component';
+import {MatCheckboxModule} from '@angular/material/checkbox';
+import {MatButtonModule} from '@angular/material/button';
+import { CustomersComponent } from './customers/customers.component';
+import {MatTableModule} from '@angular/material/table';
+import { OrdersComponent } from './orders/orders.component';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatSelectModule} from '@angular/material/select';
+import { DialogboxComponent } from './dialogbox/dialogbox.component';
+import { FormsModule } from '@angular/forms';
+import {MatDialogModule} from '@angular/material/dialog';
+import {MatSidenavModule} from '@angular/material/sidenav';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavbarComponent,
+    CustomersComponent,
+    OrdersComponent,
+    DialogboxComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatSliderModule,
+    MatCheckboxModule,
+    MatButtonModule,
+    MatTabsModule,
+    MatTableModule,
+    MatFormFieldModule,
+    MatSelectModule,
+    FormsModule,
+    MatDialogModule,
+    MatSidenavModule,
+    RouterModule.forRoot([
+
+      //{path : '', component : HomeComponent},
+      {path : 'customers', component : CustomersComponent }
+      
+      
+
+    ])
+ 
+
+  ],
+  providers: [],
+  bootstrap: [AppComponent],
+  entryComponents:[DialogboxComponent]
+})
+export class AppModule { }
